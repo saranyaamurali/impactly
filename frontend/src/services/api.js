be7d4e6611f3d38702ca72a96c2e24888254af0e
Saranyaa Murali <saranyaamurali05@gmail.com>
2026-05-10 17:23:56 +0530

    debug: log API_URL to console

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+console.log('Final API URL being used:', API_URL);
 const TIMEOUT = process.env.REACT_APP_API_TIMEOUT || 10000;
 
 // Create axios instance
